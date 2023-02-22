325412f717a21eac67cc5c9fe93c8f364644bc82
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-22 17:56:07 -0300

    feature/implement user entity and base entity

---
+++ RecordStore.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordStore.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity() { }
+
+        public int Id { get; private set; }
+    }
+
+}
---
+++ RecordStore.Core/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordStore.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, string password)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+    }
+}
