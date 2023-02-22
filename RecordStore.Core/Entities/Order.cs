e80f5984682be4d9a6b9927c87bfe8a360610d4c
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-22 18:12:53 -0300

    feature/implement order entity

---
+++ RecordStore.Core/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace RecordStore.Core.Entities
+{
+    public class Order : BaseEntity
+    {
+        public Order(int userId, int storeId, List<Record> records, decimal totalPrice)
+        {
+            UserId = userId;
+            StoreId = storeId;
+            Date = DateTime.Now;
+            Records = records;
+            TotalPrice = totalPrice;
+        }
+
+        public int UserId { get; private set; }
+        public int StoreId { get; private set; }
+        public DateTime Date { get; private set; }
+        public List<Record> Records { get; private set; }
+        public decimal TotalPrice { get; private set; }
+
+    }
+}
