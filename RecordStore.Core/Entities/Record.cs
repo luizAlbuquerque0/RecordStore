aa649583b61b1362b9733c84eab66fb1195a3548
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-22 18:04:01 -0300

    feature/implement Record and Store entities

---
+++ RecordStore.Core/Entities/Record.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordStore.Core.Entities
+{
+    public class Record : BaseEntity
+    {
+        public Record(string name, string description, string gender, decimal price, int storeId, int initialStock)
+        {
+            Name = name;
+            Description = description;
+            Gender = gender;
+            Price = price;
+            StoreId = storeId;
+            Stock = initialStock;
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Gender { get; private set; }
+        public decimal Price { get; private set; }
+        public int StoreId { get; private set; }
+        public int Stock { get; private set; }
+    }
+}
---
+++ RecordStore.Core/Entities/Store.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordStore.Core.Entities
+{
+    public class Store : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string Phone { get; private set; }
+        public List<Record> Records { get; private set; }
+
+    }
+}
