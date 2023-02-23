bcda2a703a8410f498e5bcb4374c10aea402ad4e
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-23 15:24:35 -0300

    feature/implement getAllStoreOrders query

---
+++ RecordStore.Application/Queries/GetAllStoreOrders/GetAllStoreOrdersHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using RecordStore.Application.ViewModels;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Application.Queries.GetAllStoreOrders
+{
+    public class GetAllStoreOrdersHandler : IRequestHandler<GetAllStoreOrdersQuery, List<SymplifyedRecordsViewModel>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public GetAllStoreOrdersHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+        public async Task<List<SymplifyedRecordsViewModel>> Handle(GetAllStoreOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetStoreOrdersAsync(request.StoreId);
+            return orders.Select(o => new SymplifyedRecordsViewModel(o.Id, o.TotalPrice, o.Date)).ToList();
+        }
+    }
+}
---
+++ RecordStore.Application/Queries/GetAllStoreOrders/GetAllStoreOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using RecordStore.Application.ViewModels;
+
+namespace RecordStore.Application.Queries.GetAllStoreOrders
+{
+    public class GetAllStoreOrdersQuery : IRequest<List<SymplifyedRecordsViewModel>>
+    {
+        public int StoreId { get; set; }
+    }
+}
