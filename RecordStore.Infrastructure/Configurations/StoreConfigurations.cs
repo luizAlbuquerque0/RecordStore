51458154b03aad987c273b974b9ba479046f228a
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-23 11:11:17 -0300

    feature/implement user and store db configurations

---
+++ RecordStore.Infrastructure/Configurations/StoreConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RecordStore.Core.Entities;
+
+namespace RecordStore.Infrastructure.Configurations
+{
+    public class StoreConfigurations : IEntityTypeConfiguration<Store>
+    {
+        public void Configure(EntityTypeBuilder<Store> builder)
+        {
+            builder
+                .HasKey(s => s.Id);
+        }
+    }
+}
---
+++ RecordStore.Infrastructure/Configurations/UserConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RecordStore.Core.Entities;
+
+namespace RecordStore.Infrastructure.Configurations
+{
+    public class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(u => u.Id);
+        }
+    }
+}
