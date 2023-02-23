6dfaf97aae0021166832dec8606015c9cf4978bb
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-23 12:17:40 -0300

    feature/implement store and user repository

---
+++ RecordStore.Infrastructure/Persistence/Repositories/StoreRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Entities;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Infrastructure.Persistence.Repositories
+{
+    public class StoreRepository : IStoreRepository
+    {
+        private readonly RecordStoreDbContext _dbContext;
+        public StoreRepository(RecordStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task CreateStoreAsync(Store store)
+        {
+            await _dbContext.Stores.AddAsync(store);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Store> GetByEmailAndPasswordAsync(string email, string password)
+        {
+            var store = await _dbContext.Stores.SingleOrDefaultAsync(s => s.Email == email && s.Password == password);
+            return store;
+        }
+
+        public async Task<Store> GetByIdAsync(int id)
+        {
+            var store = await _dbContext.Stores.SingleOrDefaultAsync(s => s.Id == id);
+            return store;
+        }
+    }
+}
---
+++ RecordStore.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Entities;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly RecordStoreDbContext _dbContext;
+        public UserRepository(RecordStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task CreateUserAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<User> GetUserByEmailAndPasswordAsync(string email, string password)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == password);
+            return user;
+        }
+    }
+}
