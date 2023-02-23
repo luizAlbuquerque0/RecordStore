a407cf5ee846572e404408ec1051877db7832fd7
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-23 11:07:37 -0300

    feature/implement dbContext

---
+++ RecordStore.Infrastructure/Persistence/RecordStoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Entities;
+using System.Reflection;
+
+namespace RecordStore.Infrastructure.Persistence
+{
+    public class RecordStoreDbContext : DbContext
+    {
+        public RecordStoreDbContext(DbContextOptions<RecordStoreDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; private set; }
+        public DbSet<Store> Stores { get; private set; }
+        public DbSet<Record> Records { get; private set; }
+        public DbSet<Order> Order { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
