8dbb42cce591155eb6ecefc5ceaf21e86f48d42e
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-24 14:30:37 -0300

    feature/implement updateRecordCommand

---
+++ RecordStore.Application/Commands/UpdateRecord/UpdateRecordCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace RecordStore.Application.Commands.UpdateRecord
+{
+    public class UpdateRecordCommand : IRequest<Unit>
+    {
+        public UpdateRecordCommand(int recordId, int amout)
+        {
+            RecordId = recordId;
+            Amout = amout;
+        }
+
+        public int RecordId { get; private set; }
+        public int Amout { get; private set; }
+    }
+}
---
+++ RecordStore.Application/Commands/UpdateRecord/UpdateRecordCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Application.Commands.UpdateRecord
+{
+    public class UpdateRecordCommandHandler : IRequestHandler<UpdateRecordCommand, Unit>
+    {
+        private readonly IRecordRepository _recordRepository;
+        public UpdateRecordCommandHandler(IRecordRepository recordRepository)
+        {
+            _recordRepository = recordRepository;
+        }
+        public async Task<Unit> Handle(UpdateRecordCommand request, CancellationToken cancellationToken)
+        {
+            await _recordRepository.UpdateRecordStockAsync(request.RecordId, request.Amout);
+            return Unit.Value;
+        }
+    }
+}
