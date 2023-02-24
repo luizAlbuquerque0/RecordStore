7560a415409d0deac50bdf1abcc56e7f1ce136e5
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-24 11:22:56 -0300

    feature/implement Create User Command

---
+++ RecordStore.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace RecordStore.Application.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<Unit>
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ RecordStore.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using RecordStore.Core.Entities;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Application.Commands.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+        public CreateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User(request.FullName,request.Email,request.Password,request.Phone);
+
+            await _userRepository.CreateUserAsync(user);
+
+            return Unit.Value;
+        }
+    }
+}
