e3ba473e69d51d53ffa2974ccdb5f990f2aefe0c
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-24 14:25:44 -0300

    feature/implement addRecordCommand

---
+++ RecordStore.Application/Commands/AddRecord/AddRecordCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace RecordStore.Application.Commands.AddRecord
+{
+    public class AddRecordCommand : IRequest<Unit>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Gender { get; set; }
+        public decimal Price { get; set; }
+        public int StoreId { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ RecordStore.Application/Commands/AddRecord/AddRecordCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using RecordStore.Core.Entities;
+using RecordStore.Core.Repositories;
+
+namespace RecordStore.Application.Commands.AddRecord
+{
+    public class AddRecordCommandHandler : IRequestHandler<AddRecordCommand, Unit>
+    {
+        private readonly IRecordRepository _recordRepository;
+        public AddRecordCommandHandler(IRecordRepository recordRepository)
+        {
+            _recordRepository = recordRepository;
+        }
+        public async Task<Unit> Handle(AddRecordCommand request, CancellationToken cancellationToken)
+        {
+            var record = new Record(request.Name, request.Description, request.Gender, request.Price, request.StoreId, request.Stock);
+            await _recordRepository.AddRecordAsync(record);
+            return Unit.Value;
+        }
+    }
+}
