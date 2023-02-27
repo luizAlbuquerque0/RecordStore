ae4bee2b3702da7a42f909ca3b5374b5e4e75f2c
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-27 11:09:38 -0300

    feature/implement cart and cartItem Entity

---
+++ RecordStore.Core/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace RecordStore.Core.Entities
+{
+    public class Cart : BaseEntity
+    {
+        public Cart(int userId)
+        {
+            UserId = userId;
+        }
+
+        public int UserId { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public List<CartItem> CartItem { get; private set; }
+        public User User { get; private set; }
+    }
+}
---
+++ RecordStore.Core/Entities/CartItem.cs
@@ -0,0 +1,19 @@
+﻿namespace RecordStore.Core.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        public CartItem(int cartId, int recordId, int amount)
+        {
+            CartId = cartId;
+            RecordId = recordId;
+            Amount = amount;
+        }
+
+        public int CartId { get; private set; }
+        public int RecordId { get; private set; }
+        public int Amount { get; private set; }
+        public decimal Cost { get; private set; }
+        public Cart Cart { get; private set; }
+        public Record Record { get; private set; }
+    }
+}
