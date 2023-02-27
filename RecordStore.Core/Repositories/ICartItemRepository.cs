505424b18aaeca1f372d1e6aa93304963bb433bc
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-27 11:32:56 -0300

    feature/implement cart and cartItem interface repository

---
+++ RecordStore.Core/Repositories/ICartItemRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RecordStore.Core.Repositories
+{
+    public interface ICartItemRepository
+    {
+        Task CreateCartItemAsync(int cartId, int recordId, int amount);
+        Task GetAllByCartAsync(int cartId);
+    }
+}
---
+++ RecordStore.Core/Repositories/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using RecordStore.Core.Entities;
+
+namespace RecordStore.Core.Repositories
+{
+    public interface ICartRepository
+    {
+        Task CreateCartAsync(int userId);
+        Task<Cart> GetCartAsync(int cartId);
+    }
+}
