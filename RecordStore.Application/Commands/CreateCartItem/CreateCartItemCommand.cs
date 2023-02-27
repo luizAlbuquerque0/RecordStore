b9d579ac8dc5947151f2c0246de6bfa3ac3bda5a
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-27 15:50:32 -0300

    feature/implement createCart Command

--- RecordStore.Application/Commands/CreateCartItem/CreateCartItemCommand.cs
+++ RecordStore.Application/Commands/CreateCartItem/CreateCartItemCommand.cs
@@ -4,15 +4,9 @@ namespace RecordStore.Application.Commands.CreateCartItem
 {
     public class CreateCartItemCommand : IRequest<int>
     {
-        public CreateCartItemCommand(int cartId, int recordId, int amount)
-        {
-            CartId = cartId;
-            RecordId = recordId;
-            Amount = amount;
-        }
 
-        public int CartId { get; private set; }
-        public int RecordId { get; private set; }
-        public int Amount { get; private set; }
+        public int CartId { get;  set; }
+        public int RecordId { get;  set; }
+        public int Amount { get;  set; }
     }
 }
