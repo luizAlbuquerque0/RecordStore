a86b5f6033d008e537d3049ae490fe4236cf7973
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-02-28 13:27:12 -0300

    feature/implement password hash

--- RecordStore.Application/Commands/CreateStore/CreateStoreCommandHandler.cs
+++ RecordStore.Application/Commands/CreateStore/CreateStoreCommandHandler.cs
@@ -2,19 +2,24 @@
 using RecordStore.Core.Entities;
 using RecordStore.Core.Models;
 using RecordStore.Core.Repositories;
+using RecordStore.Core.Services;
 
 namespace RecordStore.Application.Commands.CreateStore
 {
     public class CreateStoreCommandHandler : IRequestHandler<CreateStoreCommand, Unit>
     {
         private readonly IUserRepository _userRepository;
-        public CreateStoreCommandHandler(IUserRepository userRepository)
+        private readonly IAuthService _authService;
+        public CreateStoreCommandHandler(IUserRepository userRepository, IAuthService authService)
         {
             _userRepository = userRepository;
+            _authService = authService;
         }
         public async Task<Unit> Handle(CreateStoreCommand request, CancellationToken cancellationToken)
         {
-            var store = new User(request.FullName, request.Email, request.Password, request.Phone);
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            var store = new User(request.FullName, request.Email, passwordHash, request.Phone);
             store.setRole(UserRoles.user);
 
             await _userRepository.CreateUserAsync(store);
--- RecordStore.Application/Commands/CreateUser/CreateUserCommandHandler.cs
+++ RecordStore.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -2,19 +2,24 @@
 using RecordStore.Core.Entities;
 using RecordStore.Core.Models;
 using RecordStore.Core.Repositories;
+using RecordStore.Core.Services;
 
 namespace RecordStore.Application.Commands.CreateUser
 {
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
     {
         private readonly IUserRepository _userRepository;
-        public CreateUserCommandHandler(IUserRepository userRepository)
+        private readonly IAuthService _authService;
+        public CreateUserCommandHandler(IUserRepository userRepository, IAuthService authService)
         {
             _userRepository = userRepository;
+            _authService = authService;
         }
         public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = new User(request.FullName,request.Email,request.Password,request.Phone);
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            var user = new User(request.FullName,request.Email, passwordHash, request.Phone);
             user.setRole(UserRoles.user);
 
             await _userRepository.CreateUserAsync(user);
